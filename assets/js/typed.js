d0d8f023e89cbe86bcdddcaec4d86ea7eeef4324
archlinuxfanboy <lburkauskas@gmail.com>
2024-01-09 17:51:19 +0200

    changed song, formatted names better and added better snow

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,1 +1,17 @@
-new Typed('#typed',{strings:[,'hellhound.sh runs you',"fei is unlegit",'jayden is closeted', 'highkey loves femboys','eve owes fei a cleaner','rtx takes arch for fool','hoz is the worst dx9ware staff','nobody knows who arch is',],typeSpeed:35,delaySpeed:90,showCursor:true,loop:true})
+new Typed('#typed', {
+  strings: [,
+    'hellhound.sh owns you',
+    '<span style="color: #000080; text-shadow: 0 0 10px #000080, 0 0 20px #000080, 0 0 30px #000080;">miraflaff: is way too cute</span>',
+    'fei: he is not legit',
+    'jayden: secretly closeted',
+    'highkey: loves femboys',
+    'eve: codes in english',
+    'rtx: #1 eu troll (takes me for fool)',
+    'hoz: is the worst dx9ware staff',
+    '<span style="color: red;">arch:</span> main developer yet no one knows who he is'
+  ],
+  typeSpeed: 35,
+  delaySpeed: 90,
+  showCursor: true,
+  loop: true
+});
