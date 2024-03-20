fc1789bcb026c2a76ef5aaa332383247b12daacc
archlinuxfanboy <lburkauskas@gmail.com>
2024-03-20 15:53:29 +0000

    font changes

--- script.js
+++ script.js
@@ -13,33 +13,35 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 
     document.querySelector('.fade-button').addEventListener('click', function() {
-       
         this.style.opacity = 0;
         this.disabled = true;
         this.style.cursor = 'default';
 
-        
         const neonText = document.createElement('div');
         neonText.textContent = 'hellhound.sh';
         neonText.classList.add('neon-text', 'fade-in');
-        document.body.appendChild(neonText);
-
-        neonText.style.animation = 'vibrate 0.3s infinite';
-
+        
         const neonTextCSS = `
             position: fixed;
             bottom: 20px;
             left: 50%;
             transform: translateX(-50%);
             color: white;
-            font-family: Poppins;
+            font-family: 'Poppins';
             font-size: 80px;
             font-weight: bold;
-            text-shadow: 0 0 10px white, 0 0 20px white, 0 0 30px white, 0 0 40px white;
-            opacity: 1; /* Changed opacity to 1 to make it visible */
+            text-shadow: 0 0 10px rgba(255, 255, 255, 0.5), 0 0 20px rgba(255, 255, 255, 0.5), 0 0 30px rgba(255, 255, 255, 0.5), 0 0 40px rgba(255, 255, 255, 0.5);
+            opacity: 0;
+            transition: opacity 3s ease; 
         `;
 
         neonText.style.cssText = neonTextCSS;
+
+        document.body.appendChild(neonText);
+
+        setTimeout(function() {
+            neonText.style.opacity = 1; 
+        }, 100);
 
         var audio = new Audio('assets/audio.mp3');
         audio.play();
@@ -50,7 +52,6 @@ document.addEventListener('DOMContentLoaded', function() {
             namesList.classList.add('show');
         }, 100);
 
-       
         const swingingImage = document.querySelector('.swinging-image');
         swingingImage.style.opacity = 1;
 
